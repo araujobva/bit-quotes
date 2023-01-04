53e99d43746ecd8bbaef43b88a6567f0de58cc23
Bruno Araújo <araujobva@gmail.com>
2023-01-03 21:10:35 -0300

    Delete Úteis directory
    Excluindo arquivos antigos do banco de dados

--- Úteis/ScriptSQL/01 - usuario.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `bit_quotes`.`usuario` (
-  `us_nome` VARCHAR(45) NOT NULL,
-  `us_senha` VARCHAR(45) NOT NULL,
-  `us_administrador` TINYINT(1) NOT NULL,
-  PRIMARY KEY (`us_nome`))
-ENGINE = InnoDB
--- Úteis/ScriptSQL/04 - ScriptAddUsuario.sql
+++
@@ -1,9 +0,0 @@
-use bit_quotes;
-INSERT into USUARIO (us_nome, us_senha, us_administrador) VALUES ("", "", true);
-
-
-
-
-
-
-
