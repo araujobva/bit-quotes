b4b7431ad2531d4443b48585f66e6ff809013785
Bruno Araújo <araujobva@gmail.com>
2025-03-12 22:12:01 -0300

    Criando um jFileChooser e fazendo os botões Salvar e Cancelar funcionar corretamente.

--- BitQuotes/src/com/bitquotes/view/VJFrameMain.java
+++ BitQuotes/src/com/bitquotes/view/VJFrameMain.java
@@ -549,7 +549,7 @@ public class VJFrameMain extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenuItem5ActionPerformed
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
-        VJFrameBackUp screen = new VJFrameBackUp();
+        VJFrameChooseBackUpOption screen = new VJFrameChooseBackUpOption(this.user);
         screen.setVisible(true);
     }//GEN-LAST:event_jButton7ActionPerformed
 
